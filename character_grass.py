a449799047ddf32289cce970ad6b57d535793c99
TUJungMin <paulkjm92@gmail.com>
2023-09-20 17:57:55 +0900

    원 그리기 완성

--- character_grass.py
+++ character_grass.py
@@ -1,14 +1,27 @@
 from pico2d import *
 
 open_canvas()
-
-# fill here
+x =400
+y = 90
 grass = load_image('grass.png')
 character = load_image('character.png')
 
 def run_circle():
     print('Circle')
-    pass
+    
+   
+    
+    
+   
+    cx,cy,r = 400,300,200
+    for deg in (0,360,5):
+        clear_canvas_now()
+        x = cx+r*math.cos(math.radians(deg))
+        y = cy+r*math.sin(math.radians(deg))
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        delay(0.01)
+   
 
 
 def run_rectangle():
@@ -21,4 +34,4 @@ while (True):
     run_circle()
     break;
    
-close_canvas()
+
