96b88ce31b3a343fe23bb1ae34323e42ab448ba3
TUJungMin <paulkjm92@gmail.com>
2023-09-20 18:13:09 +0900

    render_all 함수 구현~

--- character_grass.py
+++ character_grass.py
@@ -10,16 +10,23 @@ character=load_image('character.png')
 x=0
 y=90
 
+def render_all(x,y):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    delay(0.02)
+
 def run_rectagle():
     
     print("rectagle")
 
     #아랫쪽 무빙~
-    for x in range(50,750,10):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        delay(0.02)
+    for x in range(50,780,10):
+        render_all(x,y)
+
+    for x in range(750,50,-10):
+        render_all(x,90)
+        
     pass
 
 def run_circle():
@@ -27,15 +34,12 @@ def run_circle():
     for deg in range(0,360,5):
         x=cx+r*math.cos(deg/360*2*math.pi)
         y=cy+r*math.sin(deg/360*2*math.pi)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        delay(0.02)
+        render_all(x,y)
 
 while (True):
     
     run_rectagle()
-    #run_circle()
+    run_circle()
     break;
     
 
